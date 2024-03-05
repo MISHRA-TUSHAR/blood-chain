f5dee557f7295a9a15d04a7496c5202cdc2aa4a9
Kshitiz Sharma <kshitiz01sharma@gmail.com>
2024-03-05 22:52:25 +0530

    ui dikhra h

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-app.use("/api/v1", ApiRoutes);
+app.use("/", ApiRoutes);
 // app.use("/api/v1/auth", );
 // app.use("/api/v1/inventory", );
 // app.use("/api/v1/analytics", );
--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Form from "../../components/shared/Form/Form";
 import { useSelector } from "react-redux";
 import Spinner from "../../components/shared/Spinner";
-
+import image from "../../assets/images/banner1.png"
 const Register = () => {
   const { loading, error } = useSelector((state) => state.auth);
   return (
@@ -13,7 +13,7 @@ const Register = () => {
       ) : (
         <div className="row g-0">
           <div className="col-md-8 form-banner ">
-            <img src="./assets/images/banner2.jpg" alt="registerImage" />
+            <img height="100"  width="100" src={image} alt="age1" />
           </div>
           <div className="col-md-4 form-container">
             <Form
