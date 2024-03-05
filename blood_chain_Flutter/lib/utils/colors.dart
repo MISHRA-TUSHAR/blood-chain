c84e0d7ce98bc8175a0ffe078b37888fbf819d65
TUSHAR MISHRA <mishratushar8890@gmail.com>
2024-03-05 16:16:33 +0530

    added flutter app code

---
+++ blood_chain_Flutter/lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const mainColor = Color.fromARGB(255, 228, 39, 25);
+  static const textHolder = Color(0xFFedf0f8);
+  static const textGrey = Color(0xFFafb1b5);
+}
